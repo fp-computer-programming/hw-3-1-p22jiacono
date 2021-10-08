9c985a40404bbdb71149123d67c7996be9add859
p22jiacono <90051927+p22jiacono@users.noreply.github.com>
2021-10-07 20:20:02 -0400

    Create hw3-1-3.py
    add hw3-1-3

---
+++ hw3-1-3.py
@@ -0,0 +1,15 @@
+#Author: JTI 9/28/21
+
+number = input("Enter a number: ")
+if int(number) % 2 == 0:
+  print(number + " is a multiple of 2")
+else:
+  print(number + " is not a multiple of 2")
+if int(number) % 3 == 0:
+  print(number + " is a multiple of 3")
+else:
+  print( number + " is not a multiple of 3")
+if int(number) % 5 == 0:
+  print(number + " is a multiple of 5")
+else:
+  print(number + " is not a multiple of 5")
